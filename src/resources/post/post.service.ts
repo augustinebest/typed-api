b85e3a3a55c319c00a994c9e2073ce0ec829529b
augustinebest <augustusbest00@gmaile.com>
2022-01-26 16:35:39 +0100

    added fetch service

--- src/resources/post/post.service.ts
+++ src/resources/post/post.service.ts
@@ -6,10 +6,31 @@ class PostService {
     /**
      * create a new post
      */
-    public async create(title: string, body: string): Promise<Post> {
+    public async create(
+        title: string,
+        body: string,
+        _id: object
+    ): Promise<Post> {
         try {
-            const post = await this.post.create({ title, body });
+            const post = await this.post.create({
+                title,
+                body,
+                user: _id,
+            });
             return post;
+        } catch (e: any) {
+            throw new Error('Unable to create post');
+        }
+    }
+
+    /**
+     * fetch posts
+     */
+
+    public async fetch(id: Object): Promise<Post[]> {
+        try {
+            const posts = await this.post.find({ user: id });
+            return posts;
         } catch (e: any) {
             throw new Error('Unable to create post');
         }
