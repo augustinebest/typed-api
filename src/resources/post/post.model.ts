41705fdcd3e601f1a537ec62da0feb205657cf13
augustinebest <augustusbest00@gmaile.com>
2022-01-26 16:35:10 +0100

    added user to the document

--- src/resources/post/post.model.ts
+++ src/resources/post/post.model.ts
@@ -11,6 +11,10 @@ const PostSchemaa = new Schema(
             type: String,
             required: true,
         },
+        user: {
+            type: Schema.Types.ObjectId,
+            ref: 'User',
+        },
     },
     { timestamps: true }
 );
